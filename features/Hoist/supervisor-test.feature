c63c658ecbc6f6baa9898ea5e6b118c976e85de2
giteshgupta <gitesh.gupta@accolitedigital.com>
2024-12-05 14:51:16 +0530

    Integrated login flow automation for suervisor app

--- features/Hoist/supervisor-test.feature
+++ features/Hoist/supervisor-test.feature
@@ -6,8 +6,8 @@ Feature: Login
     And I click on the login button
 
   Scenario: Login is successful
-    When I enter the email 'maheak.mishra@nutrien.com' in the email field
-    And I enter the password 'password' in the password field
+    When I enter the email in the email field
+    And I enter the password in the password field
     Then I should be redirected to the actions page
 
 
--- src/steps/hoist/login.steps.ts
+++ src/steps/hoist/login.steps.ts
@@ -1,48 +1,43 @@
 import { IHoistWorld } from '../../support/hoist-world';
 import { Given, Then, When } from '@cucumber/cucumber';
 
+import { Browser, Page, chromium } from '@playwright/test';
+
+let browser: Browser;
+let page: Page;
+
 Given('I login as a Lanigan site-admin', async function (this: IHoistWorld) {
-  console.log('I login as a Lanigan site-admin');
-  console.log(`baseUrl: ${this.baseUrl}`);
-
-  // Stub await using Promise.resolve()
   await Promise.resolve();
-
   console.log('Login stub completed');
 });
 
 Given('I am on the login page in the DEV env', async function (this: IHoistWorld) {
-  const page = this.page!;
-  await page.goto('https://dev.minesight.nutrien.com/');
-  console.log('Login stub DEV env completed');
+  browser = await chromium.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto('https://dev.minesight.nutrien.com');
 });
 
 Given('I click on the login button', async function (this: IHoistWorld) {
-  const page = this.page!;
   await page.getByRole('button', { name: 'Login' }).click();
   await Promise.resolve();
-  console.log('Login button clicked');
 });
 
-When('I enter the email \'maheak.mishra@nutrien.com\' in the email field', async function (this: IHoistWorld) {
-  const page = this.page!;
-  await page.getByPlaceholder('someone@example.com').click();
-  await page.getByPlaceholder('someone@example.com').fill('maheak.mishra@nutrien.com');
-  await page.getByPlaceholder('someone@example.com').press('Enter');
+When('I enter the email in the email field', async function (this: IHoistWorld) {
+  await page.locator('#i0116').click();
+  await page.locator('#i0116').fill('some.example@nutrien.com');
+  await page.getByRole('button', { name: 'Next' }).click();
 });
 
-When('I enter the password \'password\' in the password field', async function (this: IHoistWorld) {
-  const page = this.page!;
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('password');
+When('I enter the password in the password field', async function (this: IHoistWorld) {
+  await page.locator('#i0118').click();
+  await page.locator('#i0118').fill('***************');
   await page.getByRole('button', { name: 'Sign in' }).click();
+
+  await page.getByRole('button', { name: 'Yes' }).click();
   console.log('Login stub completed');
 });
 
-Then('I should be redirected to the actions page',  function (this: IHoistWorld) {
-  const page = this.page!;
-  console.log(page);
+Then('I should be redirected to the actions page', async function (this: IHoistWorld) {
+  await Promise.resolve();
   console.log('I should be redirected to the actions page stub completed');
 });
-
-
